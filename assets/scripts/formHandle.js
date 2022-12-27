9ab9e2a0780f18fab1b4eb776c3633d7ded4e71c
constadry <behappydtworry@gmail.com>
2022-12-27 14:13:40 +0300

    fixies

--- assets/scripts/formHandle.js
+++ assets/scripts/formHandle.js
@@ -1,12 +1,21 @@
 function serializeForm(formNode) {
-    const { elements } = formNode
-    Array.from(elements)
+    const { elements } = formNode;
+    let post = new Object();
+    dataArr = Array.from(elements)
         .filter((item) => !!item.name)
         .forEach((element) => {
           const { name, value } = element;
-          localStorage.setItem(name, value);
-          console.log({ name, value });
-    })
+          if (name === "post_image") {
+            post[name] = element.getAttribute('base64');
+          }
+          else
+            post[name] = value;
+    });
+
+    let oldPosts = JSON.parse(localStorage.getItem("posts"));
+    if (oldPosts === null) oldPosts = [];
+    oldPosts.push(post);
+    localStorage.setItem("posts", JSON.stringify(oldPosts));
 }
 
 function handleFormSubmit(event) {
